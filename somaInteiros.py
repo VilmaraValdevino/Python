125a97897c3a87c6fb8f334f51046fb4c9515b18
Vilmara de Oliveira Valdevino <vilmaravaldevino@globomail.com>
2020-05-08 20:40:57 -0300

    somaInteiros
    Programa que solicita dois números inteiros e imprime a soma desses dois números

---
+++ somaInteiros.py
@@ -0,0 +1,6 @@
+#Programa que pede dois números inteiros como input, e retorna imprimindo na tela o resultado da soma desses dois números
+
+n1 = int(input('Insira o 1º número: '))
+n2 = int(input('Insira o 2º número: '))
+
+print( n1 + n2 )
